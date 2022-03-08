2c25ec3f738889c7f925e1a2383ca05328ee498a
Daniel Soares <danielandradesoares@hotmail.com>
2022-03-08 15:00:20 -0300

    Apollo client setup

--- client/index.js
+++ client/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({ cache: new InMemoryCache() });
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
